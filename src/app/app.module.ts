a7e73678c02eb89cde6af052054e310ded319269
Reilly kennon <reilly@Reillys-MacBook-Pro.local>
2017-08-12 12:11:37 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'; // Add this!
+import { HttpModule } from '@angular/http'; // Add this!
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // Also here!
+    HttpModule // Also here!
   ],
   providers: [],
   bootstrap: [AppComponent]
